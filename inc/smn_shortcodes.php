8a14c9622029bbbcfbbe5bc76f927fe7a689fd4e
Fernando Blasco <fernando@sumun.net>
2025-03-31 15:15:27 +0200

    current term field

--- inc/smn_shortcodes.php
+++ inc/smn_shortcodes.php
@@ -5,3 +5,20 @@ add_action( 'acf/init', 'set_acf_settings' );
 function set_acf_settings() {
     acf_update_setting( 'enable_shortcode', true );
 }
+
+add_shortcode('current_term_field', function($atts) {
+    $atts = shortcode_atts(array(
+        'field' => '',
+    ), $atts);
+
+    if (empty($atts['field'])) {
+        return '';
+    }
+
+    $term = get_queried_object();
+    if (!$term || !isset($term->term_id)) {
+        return '';
+    }
+
+    return get_field($atts['field'], $term);
+});
