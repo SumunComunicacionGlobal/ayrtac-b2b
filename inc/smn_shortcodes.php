f140137962dfbc63d705e5c7abd543dcbbc4be00
Fernando Blasco <fernando@sumun.net>
2025-03-31 15:18:08 +0200

    current term field

--- inc/smn_shortcodes.php
+++ inc/smn_shortcodes.php
@@ -20,5 +20,13 @@ add_shortcode('current_term_field', function($atts) {
         return '';
     }
 
+    $field = get_field_object($atts['field'], $term);
+    if ($field && $field['type'] === 'file') {
+        $file_id = get_field($atts['field'], $term);
+        if ($file_id) {
+            return wp_get_attachment_image($file_id, 'full');
+        }
+    }
+
     return get_field($atts['field'], $term);
 });
