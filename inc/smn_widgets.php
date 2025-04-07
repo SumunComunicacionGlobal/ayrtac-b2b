2b0f8461339a1af2642e6f1f1f91837b63325311
Fernando Blasco <fernando@sumun.net>
2025-04-07 16:12:11 +0200

    register sidebar

---
+++ inc/smn_widgets.php
@@ -0,0 +1,17 @@
+<?php
+// Exit if accessed directly
+if ( ! defined( 'ABSPATH' ) ) {
+    exit;
+}
+
+function ayrtac_register_sidebars() {
+    register_sidebar( array(
+        'name'          => __( 'CTA Contacto Ficha Producto', 'ayrtac' ),
+        'id'            => 'cta-contacto-ficha-producto',
+        'before_widget' => '<div id="%1$s" class="widget %2$s">',
+        'after_widget'  => '</div>',
+        'before_title'  => '<h3 class="widget-title">',
+        'after_title'   => '</h3>',
+    ) );
+}
+add_action( 'widgets_init', 'ayrtac_register_sidebars' );
